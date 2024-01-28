5e36e91feb527eb7bd908a17686ae88d36d08575
ali hashemi <AGHashemi.1382@gmail.com>
2024-01-28 23:05:55 +0330

    grid struct added in grid.c

--- grid.c
+++ grid.c
@@ -16,6 +16,11 @@ typedef struct square
 
 } square_t;
 
+typedef struct grid
+{
+    square_t *squares[SQUARE_HEIGHT][SQUARE_WIDTH];
+} grid_t;
+
 square_t *create_square(int x1, int y1, int x2, int y2)
 {
     square_t *s = (square_t *)malloc(sizeof(square_t));
@@ -29,9 +34,48 @@ square_t *create_square(int x1, int y1, int x2, int y2)
     return s;
 }
 
+void destroy_square(square_t *s)
+{
+    free(s);
+}
 void draw_square(square_t *s)
 {
     al_draw_rectangle(s->x1, s->y1, s->x2, s->y2, al_map_rgb(s->r, s->g, s->b), 4);
+}
+
+grid_t *create_grid()
+{
+    grid_t *g = (grid_t *)malloc(sizeof(grid_t));
+    for (size_t i = 0; i < 15; ++i)
+    {
+        for (size_t j = 0; j < 15; ++j)
+        {
+            g->squares[i][j] = create_square(j * SQUARE_WIDTH, i * SQUARE_HEIGHT, (j + 1) * SQUARE_WIDTH, (i + 1) * SQUARE_HEIGHT);
+        }
+    }
+    return g;
+}
+
+void destroy_grid(grid_t *g)
+{
+    for (size_t i = 0; i < 15; ++i)
+    {
+        for (size_t j = 0; j < 15; ++j)
+        {
+            destroy_square(g->squares[i][j]);
+        }
+    }
+    free(g);
+}
+void draw_grid(grid_t *g)
+{
+    for (size_t i = 0; i < 15; ++i)
+    {
+        for (size_t j = 0; j < 15; ++j)
+        {
+            draw_square(g->squares[i][j]);
+        }
+    }
 }
 int main()
 {
@@ -47,16 +91,9 @@ int main()
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
-    square_t *g[SQUARE_HEIGHT][SQUARE_WIDTH];
     square_t *s = create_square(0, 0, SQUARE_WIDTH, SQUARE_HEIGHT);
 
-    for (size_t i = 0; i < SQUARE_HEIGHT ; ++i)
-    {
-        for (size_t j = 0; j < SQUARE_WIDTH ; ++j)
-        {
-            g[i][j] = create_square(j * SQUARE_WIDTH, i * SQUARE_HEIGHT, (j + 1) * SQUARE_WIDTH, (i + 1) * SQUARE_HEIGHT);
-        }
-    }
+    grid_t *g = create_grid();
 
     al_start_timer(timer);
     while (1)
@@ -70,19 +107,14 @@ int main()
             break;
         case ALLEGRO_EVENT_TIMER:
             al_clear_to_color(al_map_rgb(190, 156, 84));
-            for (size_t i = 0; i < SQUARE_HEIGHT; ++i)
-            {
-                for (size_t j = 0; j < SQUARE_WIDTH ; ++j)
-                {
-                    draw_square(g[i][j]);
-                }
-            }
-            //draw_square(s);
+            draw_grid(g);
+            // draw_square(s);
             al_flip_display();
             break;
         default:
             break;
         }
     }
+    destroy_grid(g);
     return 0;
 }
