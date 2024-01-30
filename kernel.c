5b7c28c9a3b8b71bdca0beccd16546f75f24f964
MrAliM10 <civmes10v@gmail.com>
2024-01-30 06:48:18 +0330

    nothing

--- kernel.c
+++ kernel.c
@@ -1,1 +1,32 @@
+#include "kernel.h"
+grid_t* map_grid = NULL;
 
+
+void turn(cat_t* cat) {
+
+}
+
+void game(grid_t* map) {
+    map_grid = map;
+    cat_t* cat_one = create_cat();
+    cat_t* cat_two = create_cat();
+    dog_t* dogs[4];//Weakest to Strongest:0->3
+    mouse_t* mice[18];
+    for(int i = 0 ; i < 4 ; i++)
+        dogs[i] = create_dog(i);
+    for(int i = 0 ; i < 4 ; i++) {
+        mice[i] = create_mouse(THREE);
+    }
+    for(int i = 4 ; i < 10 ; i++) {
+        mice[i] = create_mouse(TWO);
+    }
+    for(int i = 10 ; i < 18 ; i++) {
+        mice[i] = create_mouse(ONE);
+    }
+    while(1) {
+        turn(cat_one);
+        turn(cat_two);
+
+    }
+
+}
--- kernel.h
+++ kernel.h
@@ -1,9 +1,11 @@
 #ifndef TOM_JERRY_KERNEL_H
 #define TOM_JERRY_KERNEL_H
+#include "Characters.h"
 
 
-void turn(int turn);
 
+void turn(cat_t* cat);
+void game();
 
 
 #endif //TOM_JERRY_KERNEL_H
